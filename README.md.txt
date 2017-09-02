85a1201f939a3d39ca82661b660d3d56b1290292
Saniyah Shaikh <saniyah-shaikh@users.noreply.github.com>
2017-09-02 17:49:32 -0400

    Add files via upload

---
+++ README.md.txt
@@ -0,0 +1,8 @@
+# exploring-neural-nets
+
+A repo for code created to explore what neural networks are, how they work, and how to build one. 
+Created 9/2/2017
+
+
+
+Source for original code: https://medium.com/technology-invention-and-more/how-to-build-a-simple-neural-network-in-9-lines-of-python-code-cc8f23647ca1
---
+++ utilities.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  2 16:01:17 2017
+
+@author: Saniyah
+"""
+
+# file to create multiplaction training file for neural net
+    
+def write_mult_testdata(filename, x, y):
+    file = open(filename,'w')
+    for x_num in range(1, x + 1):
+        for y_num in range(y):
+            s = str(x_num) + ' ' + str(y_num) + ' ' + str(x_num * y_num) + '\n'
+            file.write(s)
+            
+def write_add_testdata(filename, x, y):
+    file = open(filename,'w')
+    for x_num in range(1, x + 1):
+        for y_num in range(y):
+            s = str(x_num) + ' ' + str(y_num) + ' ' + str(x_num + y_num) + '\n'
+            file.write(s)
+            
+write_add_testdata("testdata-add-2-10.txt", 2, 10)
