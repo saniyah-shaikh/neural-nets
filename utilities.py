1256722f6dbc8416ec3f48838682791c0a7dd6b4
Saniyah Shaikh <saniyah@seas.upenn.edu>
2017-09-20 16:43:20 -0400

    Created subtraction data maker

--- utilities.py
+++ utilities.py
@@ -21,4 +21,11 @@ def write_add_testdata(filename, x, y):
             s = str(x_num) + ' ' + str(y_num) + ' ' + str(x_num + y_num) + '\n'
             file.write(s)
             
-write_add_testdata("testdata-add-20-20.txt", 20, 20)
+def write_sub_testdata(filename, x, y):
+    file = open(filename,'w')
+    for x_num in range(1, x + 1):
+        for y_num in range(y):
+            s = str(x_num) + ' ' + str(y_num) + ' ' + str(x_num - y_num) + '\n'
+            file.write(s)
+            
+write_sub_testdata("testdata-sub-5-10.txt", 5, 10)
